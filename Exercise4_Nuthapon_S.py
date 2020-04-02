85390964d628a8b19f9985705e7a5411c5416dac
Nuthapon.S <60934066+Nathapons@users.noreply.github.com>
2020-04-02 22:34:21 +0700

    Exercise4_Nuthapon.S
    Send Exercise 4 to Python programing

---
+++ Exercise4_Nuthapon_S.py
@@ -0,0 +1,10 @@
+FoundationEnglish = float(input("Please input Foundation English score: "))
+GeneralBusiness = float(input("Please input General Business score: "))
+IntroductionToComputerSystems = float(input("Please input Introduction to Computer Systems score: "))
+ComputerProgramming = float(input("Please input Introduction to Computer Programming score: "))
+
+print("--- Your Score ---")
+print("Foundation English : ", FoundationEnglish)
+print("General Business : ", GeneralBusiness)
+print("Introduction to Computer Systems : ", IntroductionToComputerSystems)
+print("Computer Programming : ", ComputerProgramming)
